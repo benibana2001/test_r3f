794db11fa95207a6484a30d9872674bd25ebb952
benibana2001 <benibana2001@gmail.com>
2023-04-22 21:00:24 +0900

    add animation to room model

--- src/Room.jsx
+++ src/Room.jsx
@@ -1,19 +1,18 @@
 import { useAnimations, useGLTF } from "@react-three/drei";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
 export default function Room() {
-  const fox = useGLTF("./RoomCompressed.glb");
-  const animations = useAnimations(fox.animations, fox.scene);
+  const room = useGLTF("./RoomCompressed.glb");
+  const animations = useAnimations(room.animations, room.scene);
 
-  // useEffect(() => {
-  //   animations.actions["HeartAction"].reset().fadeIn(0.5).play();
-  //   animations.actions["SmallHeart01Action"].reset().fadeIn(0.5).play();
-  //   animations.actions["SmallHeart02Action"].reset().fadeIn(0.5).play();
-  // }, []);
+  useEffect(() => {
+    console.log(animations.actions);
+    animations.actions["ChairAction"].reset().fadeIn(0.5).play();
+  }, []);
 
   return (
     <primitive
-      object={fox.scene}
+      object={room.scene}
       scale={0.5}
       position={[0, 0, 0]}
       position-y={-1.0}
